ac4607e8348743bd986450d6cd8172960ba7e105
unknown <dabrowskidaniel006@gmail.com>
2020-01-18 14:56:03 +0100

    Updates

---
+++ precache-manifest.6b75fc39b1b4ef328c7687cf155a81eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3364532a190ca88d0e93339e479425a0",
+    "url": "/korean-practice/index.html"
+  },
+  {
+    "revision": "ef37b8e581db206cd29e",
+    "url": "/korean-practice/static/css/2.dd20c0da.chunk.css"
+  },
+  {
+    "revision": "f3d4f5c8f99695ddffab",
+    "url": "/korean-practice/static/css/main.7e6f9f05.chunk.css"
+  },
+  {
+    "revision": "ef37b8e581db206cd29e",
+    "url": "/korean-practice/static/js/2.17104feb.chunk.js"
+  },
+  {
+    "revision": "f3d4f5c8f99695ddffab",
+    "url": "/korean-practice/static/js/main.5b81c11b.chunk.js"
+  },
+  {
+    "revision": "0c21e06fc8a93a1f361c",
+    "url": "/korean-practice/static/js/runtime-main.908e324b.js"
+  }
+]);
